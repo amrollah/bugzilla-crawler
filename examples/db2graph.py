f3e0e60fb1f35573b449518dc434ab1a855e6311
amrollah <a.seif67@gmail.com>
2016-11-17 15:48:24 +0100

    ver1 of db2graph finished.

--- examples/db2graph.py
+++ examples/db2graph.py
@@ -12,8 +12,8 @@ import sys
 
 def valid_date(s):
     try:
-        return datetime.strptime(s, "%Y-%m-%d")
-        # return s
+        datetime.strptime(s, "%Y-%m-%d")
+        return s
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
@@ -34,7 +34,7 @@ args = parser.parse_args()
 
 # print(args.start_date)
 db_path = "{0}{1}.db".format(db_dir, args.db)
-edges = set()
+edges = []
 cur = None
 try:
     conn = sqlite3.connect(db_path)
@@ -42,11 +42,41 @@ try:
 except Exception, e:
     print(repr(e))
     exit(1)
-cur.execute('SELECT * FROM comments WHERE strftime("%Y-%m-%d", creation_time) BETWEEN ? AND ?', (args.start_date,args.end_date))
+cur.execute("SELECT * FROM comments WHERE creation_time BETWEEN ? AND ? ORDER BY bug_id ASC, creation_time ASC", (args.start_date,args.end_date))
 res = cur.fetchall()
+
+last_commenter = None
+cur_bug = None
+cur_bug_reporter = None
+cur_bug_commenters = set()
 for comment in res:
-    print comment
-    edges.add("1 2")
+    commenter_user = comment[2]
+    if comment[1] != cur_bug:
+        try:
+            cur.execute("SELECT creator_user_id FROM bugs WHERE id = ?", (comment[1],))
+            cur_bug_reporter = cur.fetchone()[0]
+        except Exception, e:
+            print(repr(e))
+            print("Error occurred in loading the bug for comment {0}".format(comment[0]))
+        cur_bug = comment[1]
+        last_commenter = None
+        cur_bug_commenters = set()
+    if not args.include_submitter and commenter_user == cur_bug_reporter:
+        continue
+    if args.clique:
+        if commenter_user not in cur_bug_commenters:
+            for commenter in cur_bug_commenters:
+                edge = "{0} {1}\n".format(commenter, commenter_user)
+                if edge not in edges:
+                    edges.append(edge)
+            cur_bug_commenters.add(commenter_user)
+    else:
+        if last_commenter and last_commenter!=commenter_user:
+            edge = "{0} {1}\n".format(last_commenter, commenter_user)
+            if edge not in edges:
+                edges.append(edge)
+        last_commenter = commenter_user
+
 # load all the comments between start and end date. Sort them by bug_id first and date second.
 # Iterate over comments from first row:
 #     initiate the last_commenter as None, initiate set of commeters for current bug as empty set.
@@ -60,5 +90,6 @@ for comment in res:
 #          update the last_commenter to the current one.
 
 with open("{0}.edges".format(args.db), 'w+') as edges_file:
-    for edge in edges:
-        edges_file.write(edge)
+    edges_file.writelines(edges)
+with open("comments.txt", 'w+') as comments_file:
+    comments_file.writelines(["{0}\n".format(cmt) for cmt in res])
